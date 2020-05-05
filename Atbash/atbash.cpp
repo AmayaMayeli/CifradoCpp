81d762d7c7c82f90163dbda0a6525946acd65a23
Mayeli Pérez <61212168+AmayaMayeli@users.noreply.github.com>
2020-05-05 09:41:28 -0600

    Update atbash.cpp

--- Atbash/atbash.cpp
+++ Atbash/atbash.cpp
@@ -7,25 +7,26 @@ int main(){
 
     string mensaje;
     int i=0;
+    int seguir = 1;
 
     cout<<"----CIFRADO ATBASH----"<<endl;
-    cout<<"Ingresa un mensaje"<<endl;
-    getline(cin, mensaje);
-    cout<<"Mensaje cifrado: ";
+        cout<<"Ingresa un mensaje"<<endl;
+        getline(cin, mensaje);
+        cout<<"Mensaje cifrado: ";
 
-    while(mensaje[i] != '\0'){
-        char x= mensaje[i];
-        char y;
-        if (x>=65 && x<=90){
-            y = 65 +(25 - (x-65));
-        } else if (x>=97 && x<=122){
-            y = 97 +(25 - (x-97));
-        } else if (x==' '){
-            y= ' ';
+        while(mensaje[i] != '\0'){
+            char x= mensaje[i];
+            char y;
+            if (x>=65 && x<=90){
+                y = 65 +(25 - (x-65));
+            } else if (x>=97 && x<=122){
+                y = 97 +(25 - (x-97));
+            } else if (x==' '){
+                y= ' ';
+            }
+            cout<<y;
+            i++;
         }
-        cout<<y;
-        i++;
-    }
 
     return 0;
 }
